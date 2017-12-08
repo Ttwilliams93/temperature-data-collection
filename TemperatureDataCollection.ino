ea1091912533a493eeffd3acf914b1e8aed558db
ttwilliams93 <ttwilliams93@gmail.com>
2017-12-07 22:05:23 -0500

    Updated and Commented

--- TemperatureDataCollection.ino
+++ TemperatureDataCollection.ino
@@ -9,12 +9,13 @@
 OneWire oneWire(ONE_WIRE_BUS);
                                              // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
+
 //Create necessary variables
 
 float Temp1;
 float Temp2;
 float Temp3;
-long CurrentTime;
+float CurrentTime;
 float PrintTime=0;
 float DataPointTime;
 
@@ -22,9 +23,17 @@ void setup(void)
 {
                                            // start serial port
   Serial.begin(9600);
-  Serial.println("Begin Data Aquisition");
+  Serial.println("Begin Data Acquisition");
                                            // Start up the library
   sensors.begin();
+
+ Serial.print("Time");
+  Serial.print(", ");  
+  Serial.print("Temp1");
+  Serial.print(", ");                           
+  Serial.print("Temp2");
+  Serial.print(", ");    
+  Serial.println("Temp3");
   
   sensors.requestTemperatures();
   Temp1 = sensors.getTempCByIndex(0)*(9/5)+32;
@@ -44,15 +53,19 @@ void loop(void)
 {
 CurrentTime = millis();
                                            // call sensors.requestTemperatures() to issue a global temperature
-                                          // Send the command to get temperatures
+                                           // Send the command to get temperatures
 if(CurrentTime - PrintTime >= 10000 ){
+  //Request Temperatures
   sensors.requestTemperatures();
+  //Assign temperatures and convert to fahrenheit
   Temp1 = sensors.getTempCByIndex(0)*(9/5)+32;
   Temp2 = sensors.getTempCByIndex(1)*(9/5)+32;
   Temp3 = sensors.getTempCByIndex(2)*(9/5)+32;
   
-  DataPointTime = PrintTime/1000+10;
-  
+  //Convert Data point time into seconds
+  DataPointTime = PrintTime/1000+10;     
+
+  //Print Data
   Serial.print(DataPointTime,2);
   Serial.print(", ");  
   Serial.print(Temp1,2);
@@ -63,8 +76,7 @@ if(CurrentTime - PrintTime >= 10000 ){
   
   PrintTime = millis();
   }
-                                            // You can have more than one IC on the same bus. 
-                                            // 0 refers to the first IC on the wire
+                                           
 }
 
 
