0f47ce50c1e8c74bd3fde6a83d36133e37573dbb
ttwilliams93 <ttwilliams93@gmail.com>
2017-12-07 21:38:00 -0500

    3 Sensors, Print every 10 seconds

--- TemperatureDataCollection.ino
+++ TemperatureDataCollection.ino
@@ -1,44 +1,61 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
  
-// Data wire is plugged into pin 2 on the Arduino
+                                            // Data wire is plugged into pin 2 on the Arduino
 #define ONE_WIRE_BUS 2
  
-// Setup a oneWire instance to communicate with any OneWire devices 
-// (not just Maxim/Dallas temperature ICs)
+                                             // Setup a oneWire instance to communicate with any OneWire devices 
+                                             // (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
- 
-// Pass our oneWire reference to Dallas Temperature.
+                                             // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
+//Create necessary variables
+
 float Temp1;
-int Temp2;
-int Temp3;
+float Temp2;
+float Temp3;
+long CurrentTime;
+float PrintTime=0;
+float DataPointTime;
+
 void setup(void)
 {
-  // start serial port
+                                           // start serial port
   Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
-  // Start up the library
+  Serial.println("Begin Data Aquisition");
+                                           // Start up the library
   sensors.begin();
 }
  
  
 void loop(void)
 {
-  // call sensors.requestTemperatures() to issue a global temperature
-  // request to all devices on the bus
-  Serial.print(" Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
-
-  Serial.print("Temperature is: ");
+CurrentTime = millis();
+                                           // call sensors.requestTemperatures() to issue a global temperature
+                                          // Send the command to get temperatures
+if(CurrentTime - PrintTime >= 10000){
+  sensors.requestTemperatures();
   Temp1 = sensors.getTempCByIndex(0)*(9/5)+32;
-  Serial.print(Temp1,3); // Why "byIndex"? 
-  //Serial.print(sensors.getTempCByIndex(1));
-  //Serial.print(sensors.getTempCByIndex(2));
-
-    // You can have more than one IC on the same bus. 
-    // 0 refers to the first IC on the wire
-    delay(1000);
+  Temp2 = sensors.getTempCByIndex(1)*(9/5)+32;
+  Temp3 = sensors.getTempCByIndex(2)*(9/5)+32;
+  
+  DataPointTime = PrintTime/1000;
+  
+  Serial.print(DataPointTime,2);
+  Serial.print(", ");  
+  Serial.print(Temp1,2);
+  Serial.print(", ");                           
+  Serial.print(Temp2,2);
+  Serial.print(", ");    
+  Serial.println(Temp3, 2);
+  
+  PrintTime = millis();
+  }
+                                            // You can have more than one IC on the same bus. 
+                                            // 0 refers to the first IC on the wire
 }
+
+
+
+
 
